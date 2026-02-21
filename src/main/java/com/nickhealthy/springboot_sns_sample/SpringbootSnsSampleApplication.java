51ca458fde23ff77eb3a0c12cff98e2cf7e22e07
nick <alskadmlcraz1@gmail.com>
2026-02-21 16:30:24 +0900

    init project

---
+++ src/main/java/com/nickhealthy/springboot_sns_sample/SpringbootSnsSampleApplication.java
@@ -0,0 +1,13 @@
+package com.nickhealthy.springboot_sns_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSnsSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSnsSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nickhealthy/springboot_sns_sample/SpringbootSnsSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nickhealthy.springboot_sns_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSnsSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
