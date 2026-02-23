388bc55efb10a4eb6fb726c1863a93fda113266b
nick <alskadmlcraz1@gmail.com>
2026-02-23 17:57:26 +0900

    feat: User entity

---
+++ src/main/java/com/nickhealthy/springboot_sns_sample/domain/base/BaseEntity.java
@@ -0,0 +1,40 @@
+package com.nickhealthy.springboot_sns_sample.domain.base;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "modified_at", nullable = false)
+    private LocalDateTime modifiedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    public void delete() {
+        this.deletedAt = LocalDateTime.now();
+    }
+
+    public boolean isDeleted() {
+        return this.deletedAt != null;
+    }
+
+    public void restore() {
+        this.deletedAt = null;
+    }
+}
---
+++ src/main/java/com/nickhealthy/springboot_sns_sample/domain/user/User.java
@@ -0,0 +1,35 @@
+package com.nickhealthy.springboot_sns_sample.domain.user;
+
+import com.nickhealthy.springboot_sns_sample.domain.base.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public static User create(String username, String password) {
+        return new User(username, password);
+    }
+}
